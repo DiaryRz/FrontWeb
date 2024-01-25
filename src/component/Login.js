403e035a5879c92bc3a4713db5b5b77e44053226
DiaryRz <119029048+DiaryRz@users.noreply.github.com>
2024-01-25 21:26:53 +0100

    ajout lien

--- src/component/Login.js
+++ src/component/Login.js
@@ -34,6 +34,7 @@ const Login = () => {
                         </div>
                         <button type="submit" className="btn btn-success" style={{width:'300px',marginTop:'30px'}}>Login</button>
                     </form>
+                    <li className='Chaque'><Link to="/insertionModel">Inserer Model</Link></li>
                 </div>
 
         </>
