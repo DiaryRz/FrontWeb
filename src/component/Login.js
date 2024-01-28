797ea9731c9f15dbee23108c20cd8f4f39773696
DiaryRz <119029048+DiaryRz@users.noreply.github.com>
2024-01-28 18:40:32 +0100

    ok

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,40 +1,42 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-    const [email, setemail] = useState('');
+    const [nomUtilisateur, setnomUtilisateur] = useState('');
     const [mdp, setmdp] = useState(''); 
-    const history = useHistory();
+    const navigate = useNavigate();
 
     const Inserer = async (e) => {
         e.preventDefault();
-        const lieu = { email, mdp }; 
-
+        const lieu = { nomUtilisateur, mdp }; 
+    
         try {
             const response = await fetch('http://localhost:8081/utilisateurs/verifierUser', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(lieu),
             });
-
+    
             if (!response.ok) {
                 throw new Error(`Erreur lors de la requête : ${response.status}`);
-            }
-            else{
-                localStorage.setItem("token" , response.data)
+            } else {
+                const responseData = await response.text();
+                
+                localStorage.setItem("token", responseData);
                 console.log(localStorage.getItem("token"));
-                history.push('/insertionModel');
+                navigate('/insertionModel');
             }
         } catch (error) {
             console.error('Une erreur s\'est produite lors de la requête :', error.message);
             // Vous pouvez ajouter ici la gestion de l'erreur, par exemple afficher un message à l'utilisateur.
         }
-    };  
+    };
+    
 
     return (
         <form onSubmit={Inserer}>
-            <label>Email: 
-                <input type="text" value={email} onChange={(e) => setemail(e.target.value)} />
+            <label>nomUtilisateur: 
+                <input type="text" value={nomUtilisateur} onChange={(e) => setnomUtilisateur(e.target.value)} />
             </label>
             <label>Mot de passe: 
                 <input type="password" value={mdp} onChange={(e) => setmdp(e.target.value)} />
